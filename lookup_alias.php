562bfb5ab3b76790d0cd30aa1c4670c18a7786eb
mgandham <manugandham@gmail.com>
2016-02-01 21:56:27 -0500

    create sign up php

--- lookup_alias.php
+++ lookup_alias.php
@@ -2,20 +2,17 @@
 <body>
 <?php
 $t = /* set to boolean indicating if alias is claimed */ 0;
-
+//Welcome test3 <?php echo $_GET["alias"]; ?><br>
 if ($t == true){ // alias is claimed
-    header("Location: profile.php");
+    header("Location: profile.php"); die();
 } else { // alias is unclaimed, display signup form
 }
+?>
+<form action="sign_up.php">
+    Choose password: <input type="password" name="psw"><br>
+    <input type="submit">
+</form>
 
-
-// mysql lookup if alias is unclaimed, display the claim form
-// 
-// else
-
-//
-//
-Welcome test3 <?php echo $_GET["alias"]; ?><br>
   
 </body>
 </html>
