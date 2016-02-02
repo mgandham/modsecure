388f951fcea7c1f3a020286a6df8f114518dae52
mgandham <manugandham@gmail.com>
2016-02-01 21:45:07 -0500

    outline lookup

--- lookup_alias.php
+++ lookup_alias.php
@@ -1,7 +1,21 @@
 <html>
 <body>
+<?php
+$t = /* set to boolean indicating if alias is claimed */ 0;
 
+if ($t == true){ // alias is claimed
+    header("Location: profile.php");
+} else { // alias is unclaimed, display signup form
+}
+
+
+// mysql lookup if alias is unclaimed, display the claim form
+// 
+// else
+
+//
+//
 Welcome test3 <?php echo $_GET["alias"]; ?><br>
-
+  
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -1,8 +1,5 @@
-// check if the alias is assigned
-//
-// if it isn't, display a box to create a key
-//
-// if it is, query the table and populate profile
-//
+// query the mysql for "alias"
+// 
+// populate from the table
 //
 //
